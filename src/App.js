fa50e39b1e0dd4da73ef6641e09cce971b284522
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2022-07-11 18:43:11 +0300

    third try

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Info from "./info";
+import About from "./About";
+import Interests from "./Interests";
+import Footer from "./Footer";
+
+
+function App() {
+  return (
+      <div className="App">
+        <Info />
+        <About />
+        <Interests />
+        <Footer />
+      </div>
+    ); 
+  } 
+   
+  
+export default App;
