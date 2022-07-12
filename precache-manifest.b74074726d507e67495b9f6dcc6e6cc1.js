ede37d411f82bb4f503f91b6b4f9b7690cd837de
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2022-07-12 12:43:52 +0300

    Updates

---
+++ precache-manifest.b74074726d507e67495b9f6dcc6e6cc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f5daf0971091528d4da",
+    "url": "/My-CV/static/css/main.191a5be6.chunk.css"
+  },
+  {
+    "revision": "7f5daf0971091528d4da",
+    "url": "/My-CV/static/js/main.7f5daf09.chunk.js"
+  },
+  {
+    "revision": "7cf0dc0d0ee933bec4f4",
+    "url": "/My-CV/static/js/1.7cf0dc0d.chunk.js"
+  },
+  {
+    "revision": "c265f017c775253f52ee",
+    "url": "/My-CV/static/js/runtime~main.c265f017.js"
+  },
+  {
+    "revision": "4b919dac6da498f00e2cd4bcebb5a180",
+    "url": "/My-CV/static/media/profile.4b919dac.jpeg"
+  },
+  {
+    "revision": "39e0aa671540d6f1d8c531e595b99b86",
+    "url": "/My-CV/static/media/bck.39e0aa67.png"
+  },
+  {
+    "revision": "b33f40e53791625a053fc8aefe809338",
+    "url": "/My-CV/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CV-third-try/precache-manifest.01a2c07fd2661f913bc0aca676578eb2.js"
+  "/My-CV/precache-manifest.b74074726d507e67495b9f6dcc6e6cc1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/CV-third-try/index.html", {
+workbox.routing.registerNavigationRoute("/My-CV/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
